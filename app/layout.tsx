db685bff1a31b67b06a275b7dd63b08ef960c101
shreyaan22 <shreyaan@astratmutual.com>
2025-08-07 19:06:53 +0530

    Google Tag Manager Setup
    Added Google Tag (GTM-KDQ3GFCV) integration to RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import localFonts from "next/font/local";
 import { Toaster } from "sonner";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -25,17 +26,44 @@ export const metadata: Metadata = {
     "Astratinvest aims for superior risk-adjusted returns through a scientific and mathematical investment model. We conduct quantitative research with rigorous backtesting utilizing high-quality data to identify potential outperformers.",
 };
 
-
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      {/* GTM Script in <head> */}
+      <head>
+        <Script
+          id="gtm-head"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-KDQ3GFCV');
+            `,
+          }}
+        />
+      </head>
+
       <body className={`${inter.className} ${ivy.variable} ${ivy_thin.variable}`}>
+        {/* GTM NoScript fallback */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KDQ3GFCV"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+
         {children}
         <Toaster closeButton richColors />
       </body>
     </html>
   );
 }
+
 
 
 // "use client"; // Required for useRouter()
