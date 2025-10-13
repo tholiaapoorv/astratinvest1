d71bb3c724c0e3078a1473d9214dcc5523e6ad77
yAshKejrIwaL2005 <kejriwalyash197@gmail.com>
2025-10-13 14:42:42 +0530

    AclChart

---
+++ app/research/case-studies/datafeed.js
@@ -0,0 +1,84 @@
+
+async function loadJson() {
+    try {
+        let response = await fetch("data_point.json");
+        let data = await response.json();
+        console.log("data fetched successfully");
+        console.log(data)
+        return data;
+    } catch (error) {
+        console.error('Error loading JSON:', error);
+    }
+}
+let jsonData = await loadJson();
+class CustomDatafeed {
+    async onReady(cb) {
+        console.log("onReady")
+        setTimeout(() => cb({
+            supported_resolutions: ["1M"],
+            supports_marks: false,
+            supports_timescale_marks: false,
+            supports_time: false,
+        }), 0)
+    }
+    async searchSymbols(userInput, exchange, symbolType, onResultReadyCallback) {
+        console.log("searchSymbols");
+        onResultReadyCallback([]);
+    }
+    async resolveSymbol(symbolName, onSymbolResolvedCallback, onResolveErrorCallback, extension) {
+        console.log("resolveSymbol")
+        try {
+            setTimeout(() => {
+                onSymbolResolvedCallback({
+                    name: 'NIFTY 50',
+                    ticker: 'NIFTY 50',
+                    description: 'NIFTY 50',
+                    type: 'stock',
+                    session: '24x7',
+                    timezone: 'Etc/UTC',
+                    exchange: 'NIFTY 50',
+                    minmov: 1,
+                    pricescale: 100,
+                    has_intraday: false,
+                    visible_plots_set: 'ohlc',
+                    has_weekly_and_monthly: false,
+                    supported_resolutions: ["1M"],
+                    volume_precision: 0,
+                    data_status: 'streaming',
+                });
+            }, 0)
+
+        }
+        catch (e) {
+            setTimeout(() => {
+                onResolveErrorCallback(e.message);
+            }, 0);
+
+        }
+    }
+    async getBars(symbolInfo, resolution, periodParams, onHistoryCallback, onErrorCallback) {
+        console.log("getBars")
+        const from = periodParams.from;
+        const to = periodParams.to;
+        const filteredBars = jsonData.filter(bar => {
+            const timeSec = Math.floor(bar.time / 1000);
+            return timeSec >= from && timeSec <= to;
+        }).map(({ mark, ...rest }) => rest);
+
+        if (filteredBars.length === 0) {
+            setTimeout(() => onHistoryCallback([], { noData: true }), 0);
+        } else if (filteredBars.length < periodParams.countBack) {
+            setTimeout(() => onHistoryCallback(filteredBars, { noData: true }), 0);
+        } else {
+            setTimeout(() => onHistoryCallback(filteredBars, { noData: false }), 0);
+        }
+    }
+    async subscribeBars(symbolInfo, resolution, onRealtimeCallback, subscriberUID, onResetCacheNeededCallback) {
+        console.log('[subscribeBars]: Real-time updates disabled for static data');
+    }
+    async unsubscribeBars(subscriberUID) {
+        console.log('[unsubscribeBars]: Real-time updates disabled for static data');
+    }
+}
+const customDatafeed = new CustomDatafeed();
+export default customDatafeed;
--- app/research/case-studies/page.tsx
+++ app/research/case-studies/page.tsx
@@ -1,19 +1,21 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { TbArrowLeft, TbArrowRight, TbArrowUpRight } from "react-icons/tb";
 import { motion } from "framer-motion";
 import axios from "axios";
 import { caseStudy } from "@/types";
 import SanityImage from "@/components/ui/SanityImage";
+import { ChartingLibraryWidgetOptions, ResolutionString, widget } from "@/public/static/charting_library";
+import customDatafeed from "./datafeed"
 import { Separator } from "@radix-ui/react-separator";
 import { PortableText, PortableTextComponents } from "next-sanity";
 import Link from "next/link";
-
 import { getImageDimensions } from "@sanity/asset-utils";
 import Heatmap from "@/components/Graphs/Heatmap";
 
 const Page = () => {
-  const [caseStudies, setCaseStudies] = useState<any[]>();
+  const containerRef = useRef<HTMLDivElement>(null);
+  const [caseStudies, setCaseStudies] = useState<any[]>([]);
   const [loading, setLoading] = useState(false);
   useEffect(() => {
     setLoading(true);
@@ -29,6 +31,65 @@ const Page = () => {
         console.log(err);
         setLoading(false);
       });
+    if (!containerRef.current) return
+    const widgetOptions: ChartingLibraryWidgetOptions = {
+      container: containerRef.current,
+      library_path: '/static/charting_library/',
+      datafeed: customDatafeed,
+      symbol: 'NIFTY 50',
+      interval: '1M' as ResolutionString,
+      locale: 'en',
+      autosize: true,
+      theme: 'light',
+      studies_overrides: {},
+      disabled_features: ["header_widget", "left_toolbar", "chart_zoom", "horz_touch_drag_scroll", "vert_touch_drag_scroll"],
+    };
+    const tvWidget = new widget(widgetOptions);
+    tvWidget.onChartReady(() => {
+      const chart = tvWidget.chart();
+      const rectangles = [
+        [1475452800000, 1483315200000],
+        [1501545600000, 1504224000000],
+        [1514764800000, 1522627200000],
+        [1538352000000, 1548979200000],
+        [1561939200000, 1564617600000],
+        [1577836800000, 1585699200000],
+        [1604275200000, 1609459200000],
+        [1630454400000, 1641168000000],
+        [1646179200000, 1654041600000],
+        [1659312000000, 1661990400000],
+        [1667260800000, 1675209600000],
+        [1693526400000, 1696291200000],
+        [1706745600000, 1709251200000],
+        [1714608000000, 1717372800000],
+        [1722470400000, 1740787200000],
+        [1754006400000, 1756684800000]
+      ]
+      chart.onDataLoaded().subscribe(
+        null,
+        () => {
+          rectangles.forEach(([r1, r2]) => {
+            chart.createMultipointShape(
+              [
+                { time: r1 / 1000, price: 0 },
+                { time: r2 / 1000, price: 1000000 },
+
+              ],
+              {
+                shape: 'rectangle',
+                disableSelection: true,
+                disableUndo: true,
+                filled: true,
+                lock: true
+              }
+            )
+          })
+        }
+      )
+    });
+    return () => {
+      tvWidget.remove();
+    };
   }, []);
   console.log(caseStudies);
 
@@ -145,14 +206,14 @@ const Page = () => {
   };
   return (
     <>
-    <motion.div
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{ duration: 0.8 }}
-    >
-      <div className="my-10 flex flex-col items-center justify-center gap-[3.5rem] py-6">
-        {/* top */}
-        {/* <div className="flex items-center justify-center gap-4">
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.8 }}
+      >
+        <div className="my-10 flex flex-col items-center justify-center gap-[3.5rem] py-6">
+          {/* top */}
+          {/* <div className="flex items-center justify-center gap-4">
           
           <div className="relative flex w-full flex-col gap-3 px-[5rem] pb-8">
             <h1 className="font-ivy text-[min(8vh,8vw)] font-bold tracking-wide">
@@ -161,95 +222,101 @@ const Page = () => {
             <hr className="border-[#000121]/10" />
           </div>
         </div> */}
-        <div className="flex flex-col items-center justify-center gap-[3.5rem]">
-          <h1 className="font-poppins font-semibold uppercase tracking-[0.35em] text-[#3959E5] phone:text-[min(4vh,4vw)] smTablet:text-[min(2.35vh,2.35vw)]">
-            Case Studies
-          </h1>
-          <p className="w-[80%] text-pretty text-center font-poppins font-light leading-relaxed tracking-wider text-[#000000] phone:text-[min(3vh,3vw)] smTablet:text-[min(2.1vh,2.1vw)]">
-            We use an amalgamation of proprietary models to prepare a complete
-            and holistic portfolio. Our models churn vast amount of data for
-            every stock for a very long duration to ensure for data inaccuracy
-            and marginal errors.
-          </p>
-        </div>
+          <div className="flex flex-col items-center justify-center gap-[3.5rem]">
+            <h1 className="font-poppins font-semibold uppercase tracking-[0.35em] text-[#3959E5] phone:text-[min(4vh,4vw)] smTablet:text-[min(2.35vh,2.35vw)]">
+              Case Studies
+            </h1>
+            <p className="w-[80%] text-pretty text-center font-poppins font-light leading-relaxed tracking-wider text-[#000000] phone:text-[min(3vh,3vw)] smTablet:text-[min(2.1vh,2.1vw)]">
+              We use an amalgamation of proprietary models to prepare a complete
+              and holistic portfolio. Our models churn vast amount of data for
+              every stock for a very long duration to ensure for data inaccuracy
+              and marginal errors.
+            </p>
+          </div>
 
-        {/* bottom */}
+          {/* bottom */}
 
-        <div>
-          <div className="grid w-full grid-cols-1 gap-[5rem] py-[2rem] pb-8 xsPhone:px-[2rem] smTablet:px-[3.5rem] smLaptop:px-[5rem]">
-            {loading ? (
-              <div>Loading...</div>
-            ) : (
-              caseStudies?.map((caseStudy, idx) => {
-                return (
-                  <div key={caseStudy._id} className="flex flex-col gap-10">
-                    <h1 className="font-ivy text-[min(4vh,4vw)] font-bold tracking-wide">
-                      {caseStudy.name}:
-                    </h1>
-                    <div className="flex w-full items-center justify-center">
-                      {caseStudy.body && (
-                        <p className="w-full">
-                          <PortableText
-                            value={caseStudy.body}
-                            components={components}
-                          />
-                        </p>
-                      )}
+          <div>
+            <div className="grid w-full grid-cols-1 gap-[5rem] py-[2rem] pb-8 xsPhone:px-[2rem] smTablet:px-[3.5rem] smLaptop:px-[5rem]">
+              {loading ? (
+                <div>Loading...</div>
+              ) : (
+                caseStudies?.map((caseStudy, idx) => {
+                  return (
+                    <div key={caseStudy._id} className="flex flex-col gap-10">
+                      <h1 className="font-ivy text-[min(4vh,4vw)] font-bold tracking-wide">
+                        {caseStudy.name}:
+                      </h1>
+                      <div className="flex w-full items-center justify-center">
+                        {caseStudy.body && (
+                          <p className="w-full">
+                            <PortableText
+                              value={caseStudy.body}
+                              components={components}
+                            />
+                          </p>
+                        )}
+                      </div>
+                      <div className="flex w-full items-center justify-center gap-10 xsPhone:flex-col tablet:flex-row">
+                        {
+                          (caseStudy.name === "Efficacy of the MSQ system" ? (
+                            <div ref={containerRef} className="h-auto w-fit xsPhone:w-full tablet:w-[50%]" />
+                          ) : (
+                            caseStudy.image2 ? (
+                              <>
+                                <SanityImage
+                                  src={caseStudy.image}
+                                  className="h-auto w-fit xsPhone:w-full tablet:w-[50%]"
+                                />
+                                <SanityImage
+                                  src={caseStudy.image2}
+                                  className="h-auto w-fit xsPhone:w-full tablet:w-[50%]"
+                                />
+                              </>
+                            ) : (
+                              <SanityImage
+                                src={caseStudy.image}
+                                className="h-auto w-fit xsPhone:w-full tablet:w-[70%]"
+                              />
+                            )
+                          ))
+                        }
+                      </div>
                     </div>
-                    <div className="flex w-full items-center justify-center gap-10 xsPhone:flex-col tablet:flex-row">
-                      {caseStudy.image2 ? (
-                        <>
-                          <SanityImage
-                            src={caseStudy.image}
-                            className="h-auto w-fit xsPhone:w-full tablet:w-[50%]"
-                          />
-                          <SanityImage
-                            src={caseStudy.image2}
-                            className="h-auto w-fit xsPhone:w-full tablet:w-[50%]"
-                          />
-                        </>
-                      ) : (
-                        <SanityImage
-                          src={caseStudy.image}
-                          className="h-auto w-fit xsPhone:w-full tablet:w-[70%]"
-                        />
-                      )}
-                    </div>
-                  </div>
-                );
-              })
-            )}
-            <div className="flex flex-col gap-10">
-              <h1 className="font-ivy text-[min(4vh,4vw)] font-bold tracking-wide">
-                Dynamic Calculative Exposure
-              </h1>
-              <div className="flex w-full items-center justify-center font-poppins font-light text-[#000121]">
-                Our dynamic models stay vigilant in pre-empting sectors where
-                outperformance can come , thereby helping us in aligning our
-                exposure and identify market beating opportunities. Our
-                strategies are designed for stability and reliability in all
-                market conditions.
-              </div>
-              <div className="flex w-full items-center justify-center gap-10 xsPhone:flex-col tablet:flex-row">
-                <Heatmap />
+                  );
+                })
+              )}
+              <div className="flex flex-col gap-10">
+                <h1 className="font-ivy text-[min(4vh,4vw)] font-bold tracking-wide">
+                  Dynamic Calculative Exposure
+                </h1>
+                <div className="flex w-full items-center justify-center font-poppins font-light text-[#000121]">
+                  Our dynamic models stay vigilant in pre-empting sectors where
+                  outperformance can come , thereby helping us in aligning our
+                  exposure and identify market beating opportunities. Our
+                  strategies are designed for stability and reliability in all
+                  market conditions.
+                </div>
+                <div className="flex w-full items-center justify-center gap-10 xsPhone:flex-col tablet:flex-row">
+                  <Heatmap />
+                </div>
               </div>
             </div>
           </div>
         </div>
+      </motion.div>
+      {/* 👇 TradingView Credit */}
+      <div className="mt-4 mb-8 text-center text-sm font-light text-gray-500">
+        Data insights powered by{" "}
+        <a
+          href="https://www.tradingview.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="font-medium text-[#3959E6] underline"
+        >
+          TradingView
+        </a>
       </div>
-    </motion.div>
-    {/* 👇 TradingView Credit */}
-    <div className="mt-4 mb-8 text-center text-sm font-light text-gray-500">
-      Data insights powered by{" "}
-      <a
-        href="https://www.tradingview.com"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="font-medium text-[#3959E6] underline"
-      >
-        TradingView
-      </a>
-    </div>
     </>
   );
 };
